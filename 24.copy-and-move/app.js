02dd56fbdeecc57d8bab15f3b83719fa3053ce74
Manokaran <manokarane@nmsworks.co.in>
2024-09-05 22:53:29 +0530

    Copy and move text from one input to another input in js has been completed

---
+++ 24.copy-and-move/app.js
@@ -0,0 +1,21 @@
+const copyBtn = document.querySelector('#copyBtn');
+const moveBtn = document.querySelector('#moveBtn');
+const copyTextArea = document.querySelector('#copy');
+const moveTextArea = document.querySelector('#move');
+const copiedContent = document.querySelector('.content-copied');
+
+copyBtn.onclick = function () {
+    copiedContent.classList.add('fade-in');
+    setTimeout(()=> {
+        copiedContent.classList.add('fade-out');
+    }, 1000);
+    setTimeout(()=> {
+        copiedContent.classList.remove('fade-in');
+        copiedContent.classList.remove('fade-out');
+    }, 2000);
+    document.execCommand("copy");
+    copyTextArea.select();
+}
+moveBtn.onclick = function() {
+    moveTextArea.value = copyTextArea.value;
+}
